2d3a86696dc166315648a87c2f8752390c9e8039
Purushotham Reddy <purushothamreddy@Purushothams-MacBook-Pro.local>
2017-11-07 17:13:25 +0530

    Added base setup for running appium tets in an avd

--- src/test/java/features/Sample.feature
+++ src/test/java/features/Sample.feature
@@ -1,5 +1,3 @@
 Feature: Registration Flow
   Scenario: As a User I should be able to signup with my google account
     Given user opens the app
-    When user chooses to logIn using Google
-    Then user should see account picker popup
--- src/test/java/stepDefs/LaunchApp.java
+++ src/test/java/stepDefs/LaunchApp.java
@@ -8,14 +8,27 @@ import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class LaunchApp {
+
     @When("user opens the app")
     public void launchApp() throws Throwable{
+
         DesiredCapabilities caps = new DesiredCapabilities();
+
         caps.setCapability("appium-version", "1.7.1");
         caps.setCapability("platformName", "Android");
         caps.setCapability("platformVersion", "6.0");
+
         caps.setCapability("deviceName", "Google Nexus 5x");
-        caps.setCapability("app", "/Users/purushothamreddy/Documents/training/appium/app/quikr.apk");
+        caps.setCapability("newCommandTimeout",120);
+
+        //Launch virtual device and wait for 120 seconds
+        caps.setCapability("avd","testAVD");
+        caps.setCapability("avdReadyTimeout",120000);
+//        caps.setCapability("app","app/quikr.apk");
+
+        caps.setCapability("appPackage","com.quikr");
+        caps.setCapability("appActivity","com.quikr.old.SplashActivity");
+
         AppiumDriver appiumDriver = new AppiumDriver(new URL("http://localhost:4723/wd/hub"), caps);
         appiumDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
